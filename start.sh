b9e2b1ce2cb84d6904fbff2a12fd003ff75b23b5
anton baranov <abaranov@linux.com>
2015-03-03 09:13:43 -0500

    gerrit: start script for gerrit

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GERRIT_DIR=/opt/gerrit/
+GERRIT_VERSION="2.10"
+
+# Clear cache dir
+rm -rf ${GERRIT_DIR}/work/cache
+# Reindex changes
+java -jar ${GERRIT_DIR}/downloads/gerrit-${GERRIT_VERSION}.jar \
+  reindex -d ${GERRIT_DIR}/work
+
+${GERRIT_DIR}/work/bin/gerrit.sh supervise -d ${GERRIT_DIR}/work
