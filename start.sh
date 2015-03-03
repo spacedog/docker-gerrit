272ce384958d492b2e96c4c848c9258fa4f706be
anton baranov <abaranov@linux.com>
2015-03-03 11:18:38 -0500

    gerrit: fixed path to gerrit war file

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ GERRIT_VERSION="2.10"
 # Clear cache dir
 rm -rf ${GERRIT_DIR}/work/cache
 # Reindex changes
-java -jar ${GERRIT_DIR}/downloads/gerrit-${GERRIT_VERSION}.jar \
+java -jar ${GERRIT_DIR}/downloads/gerrit-${GERRIT_VERSION}.war \
   reindex -d ${GERRIT_DIR}/work
 
 ${GERRIT_DIR}/work/bin/gerrit.sh supervise -d ${GERRIT_DIR}/work
